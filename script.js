1c88485f31e0e004f06c19e7156277a693ffa88a
Jorge Nieto <jorgenieto_14@hotmail.com>
2024-08-25 11:25:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const alerta = document.querySelector(".mensajeAlerta");
+const parrafoNota = document.querySelector(".nota")
+
+// `La letra "e" es convertida para "enter"`
+// `La letra "i" es convertida para "imes"`
+// `La letra "a" es convertida para "ai"`
+// `La letra "o" es convertida para "ober"`
+// `La letra "u" es convertida para "ufat"`
+
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    alerta.style.display= "none";
+    parrafoNota.style.display= "none"; 
+    
+    
+}
+
+
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+
+        }
+
+    }
+ 
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = ""
+    
+       
+}
+
+
+function desencriptar(stringDesencriptada){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+
+        }
+
+    }
+ 
+    return stringDesencriptada
+}
+
+
+
+function btnCopiar(){
+    const copiarmensaje = copiartexto(mensaje.value)
+
+}
+
+
+
+function copiartexto(stringCopiada){
+    
+
+    navigator.clipboard.writeText(mensaje.value);
+
+
+    
+}
